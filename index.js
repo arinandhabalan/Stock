bf56f6013abc300e4068840f6419c7cf908e214f
arinandhabalan <146729613+arinandhabalan@users.noreply.github.com>
2024-11-19 17:17:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TradingViewWidget from "./App";
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+Root.render(<TradingViewWidget />)
